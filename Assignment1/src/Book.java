f38b299ec8acbc27b2637b96942d04b8dc43b5b4
Shubham Mehta <69765380+Shubham-D-Mehta@users.noreply.github.com>
2021-10-17 17:01:06 -0400

    Create Book.java
    part 3

---
+++ Assignment1/src/Book.java
@@ -0,0 +1,41 @@
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class Book extends Item {
+    private ArrayList<String> authors=new ArrayList<>();
+    public Book(String title, double regularPrice, LocalDate releaseDate, int quantity, ArrayList<String> authors) {
+        super(title, regularPrice, releaseDate, quantity);
+        this.authors=authors;
+    }
+
+    public Book(Item o, int itemID, ArrayList<String> authors) {
+        super(o, itemID);
+        this.authors = authors;
+    }
+
+    public ArrayList<String> getAuthor(){
+        ArrayList<String> copy=(ArrayList<String>)authors.clone();
+        return copy;
+    }
+
+    @Override
+    public String toString(){
+        String superToString=super.toString();
+        superToString+="\n\tAuthors:";
+        for(String e:authors){
+            superToString+="\n\t "+e;
+        }
+        return superToString;
+    }
+
+    @Override
+    public double getPrice(LocalDate sellDate){
+        return this.getRegularPrice()*this.getDiscount();
+    }
+
+    public Item makeCopy(){
+        Book copy=new Book(this,this.getItemID(),authors);
+        return copy;
+    }
+}
+
