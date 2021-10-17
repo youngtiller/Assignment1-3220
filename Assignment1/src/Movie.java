6bfc2f0b4fc1fd5be1a31fdcab1b90d8317861b5
Shubham Mehta <69765380+Shubham-D-Mehta@users.noreply.github.com>
2021-10-17 17:23:30 -0400

    Update Movie.java
    changed copy constructor

--- Assignment1/src/Movie.java
+++ Assignment1/src/Movie.java
@@ -2,31 +2,31 @@ import java.time.LocalDate;
 import java.util.*;
 
 public class Movie extends Item {
-	String director;
-	String producer;
+    String director;
+    String producer;
 
-	public Movie(String title, double regularPrice, LocalDate releaseDate, int quantity, String director,
-			String producer) {
-		super(title, regularPrice, releaseDate, quantity);
-		this.director = director;
-		this.producer = producer;
-	}
+    public Movie(String title, double regularPrice, LocalDate releaseDate, int quantity, String director,
+                 String producer) {
+        super(title, regularPrice, releaseDate, quantity);
+        this.director = director;
+        this.producer = producer;
+    }
 
-	public Movie(String title, double regularPrice, LocalDate releaseDate, int quantity, int itemID, String directory,
-			String producer) {
-		super(title, regularPrice, releaseDate, quantity);
-		this.director = directory;
-		this.producer = producer;
-	}
+    public Movie(Movie o, int itemID, String directory,
+                 String producer) {
+        super(o,itemID);
+        this.director = directory;
+        this.producer = producer;
+    }
 
-	@Override
-	public String toString() {
-		return toString() + "\n\tDirectory: " + director + "\n\tProducer: " + producer;
-	}
-	
-	@Override
-	public Item makeCopy() {
-		return new Movie(getTitle(), getRegularPrice(), getReleaseDate(), getQuantity(), getItemID(), director, producer);
-	}
+    @Override
+    public String toString() {
+        return super.toString() + "\n\tDirectory: " + director + "\n\tProducer: " + producer;
+    }
+
+    @Override
+    public Item makeCopy() {
+        return new Movie(this,getItemID(), director, producer);
+    }
 
 }
