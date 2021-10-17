2f5bc89868febce8f1e8d65af98a1f1cc7dfbc38
Shubham Mehta <69765380+Shubham-D-Mehta@users.noreply.github.com>
2021-10-17 17:17:50 -0400

    Update Game.java
    changed the copy constructor

--- Assignment1/src/Game.java
+++ Assignment1/src/Game.java
@@ -2,49 +2,47 @@ import java.time.LocalDate;
 import java.util.*;
 
 public class Game extends Item {
-	String studio;
-	boolean discontinued;
+    private String studio;
+    private boolean discontinued;
 
-	public Game(String title, double regularPrice, LocalDate releaseDate, int quantity, String studio,
-			boolean discontinued) {
-		super(title, regularPrice, releaseDate, quantity);
-		this.studio = studio;
-		this.discontinued = discontinued;
-	}
+    public Game(String title, double regularPrice, LocalDate releaseDate, int quantity, String studio,
+                boolean discontinued) {
+        super(title, regularPrice, releaseDate, quantity);
+        this.studio = studio;
+        this.discontinued = discontinued;
+    }
 
-	public Game(String title, double regularPrice, LocalDate releaseDate, int quantity, int itemID, String studio,
-			boolean discontinued) {
-		super(title, regularPrice, releaseDate, quantity);
-		this.studio = studio;
-		this.discontinued = discontinued;
-	}
+    public Game(Game o, int itemID, String studio,
+                boolean discontinued) {
+        super(o,itemID);
+        this.studio = studio;
+        this.discontinued = discontinued;
+    }
 
-	public void setDiscontinued(boolean discontinued) {
-		this.discontinued = discontinued;
-	}
+    public void setDiscontinued(boolean discontinued) {
+        this.discontinued = discontinued;
+    }
 
-	public boolean getDiscontinued() {
-		return discontinued;
-	}
+    public boolean getDiscontinued() {
+        return discontinued;
+    }
 
-	@Override
-	public String toString() {
-		return toString() + "\n\tStudio: " + studio + "\n\tDiscontinued: " + discontinued;
-	}
+    @Override
+    public String toString() {
+        return super.toString() + "\n\tStudio: " + studio + "\n\tDiscontinued: " + discontinued;
+    }
 
-	@Override
-	public double getPrice(LocalDate sellDate) {
-		double price = getRegularPrice();
-		if (getDiscontinued()) {
-			price = price * 20;
-		}
-		return price;
-	}
+    @Override
+    public double getPrice(LocalDate sellDate) {
+        double price = getRegularPrice();
+        if (getDiscontinued()) {
+            price = price * 20;
+        }
+        return price;
+    }
 
-	@Override
-	public Item makeCopy() {
-		return new Game(getTitle(), getRegularPrice(), getReleaseDate(), getQuantity(), getItemID(), studio, discontinued);
-		
-	}
-
+    @Override
+    public Item makeCopy() {
+        return new Game(this,this.getItemID(),studio,discontinued);
+    }
 }
