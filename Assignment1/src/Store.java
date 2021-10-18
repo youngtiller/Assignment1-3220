5c253b2a695a71aa14481be1d4d0a185856f19b5
Harnoor Singh Reen <54823204+reenharnoorsingh@users.noreply.github.com>
2021-10-18 08:15:02 -0400

    Store updates

--- Assignment1/src/Store.java
+++ Assignment1/src/Store.java
@@ -1,94 +1,81 @@
 import java.util.*;
+import java.util.ArrayList;
+
 
 public class Store {
-	private ArrayList<Item> inventory = null;
-	private double totalSalesValue;
-	private int totalNumberOfSales;
 
-	public Store() {
-		this.inventory = new ArrayList<>();
-		this.totalSalesValue = 0;
-		this.totalNumberOfSales = 0;
+	List<Item> inventory;
+	double totalSalesValue = 0;
+	int totalNumberOfSales = 0;
+	public Store()
+	{
+		this(new ArrayList<Item>());
 	}
-
-	// addItem which is void, it takes an Item and adds it to inventory
 	public void addItem(Item item) {
 		inventory.add(item);
 	}
-
-	/*
-	 * makeSale which returns boolean, and takes as input an int itemID, an int
-	 * called quantity, and a java.time.LocalDate called sellDate. The method
-	 * returns false if the item cannot be found in the store (when calling
-	 * getItemByID, the item is null). Otherwise, it calls a method on item called
-	 * “sell”, giving the quantity and sellDate to that method, capturing its return
-	 * value called “value”. If value > 0, then it increments the stores
-	 * totalNumberOfSales and totalSalesValue accordingly, and returns true.
-	 * Otherwise, it returns false.
-	 */
-	public Boolean makeSale(int itemID, int quantity, java.time.LocalDate sellDate) {
-		Item item = getItemByID(itemID);
-		if (item == null) {
+	public boolean makeSale(Item item, int itemID, int quantity, java.time.LocalDate sellDate) {
+		item = getItemByID(itemID);
+		if(item.equals(null)) {
 			return false;
-		} else {
-			double value = item.sell(quantity, sellDate);
-			if (value > 0) {
+		}
+		else {
+			double value = item.sell(quantity, sellDate); //If item is in store the method sell will give the quanitity of said item and it's sell date.
+			if(value > 0) { //Value has to be greater than 0 for store to update and add how ever many number of sales and the sales value accumulated from said item.
 				totalNumberOfSales++;
-				totalSalesValue++;
+				totalSalesValue += value;
 				return true;
-
-			} else {
+			}
+			else {
 				return false;
 			}
-
 		}
 	}
-
-	/*
-	 * getInventoryString which returns String, which iterates over the Store’s
-	 * inventory and for each item adds to a String “\n” + item.toString() + “\n”.
-	 * It then returns this string,
-	 */
+	//Method getInventoryString that takes nothing it will go through the stores inventory and add items to our data.
 	public String getInventoryString() {
-		String x = "";
-		for (int i = 0; i < inventory.size(); i++) {
-			x = x + "\n" + item.toString() + "\n";
+		String data = ""; //We set data basically to a null value and add item within the inventory to this string.
+		for(Item item: inventory) {
+			data += "\n" + item.toString() + "\n";
 		}
-		return x;
+		return data;
 	}
-
-	/*
-	 * listInventory which is void, which simply prints into the console “Here are
-	 * all the inventory items:” followed by the output from getInventoryString.
-	 */
+	//Method listInventory which takes nothing we are simply printing a statement and the inventory in the store
 	public void listInventory() {
-
+		System.out.println("Here are all the inventory items:\n");
+		System.out.println(getInventoryString());
 	}
-
-	/*
-	 * checkForItem which returns Item and takes an int called itemID. It iterates
-	 * over the inventory and if it finds an Item with the given ID, it returns a
-	 * copy of the item. Otherwise, it returns null. This method is public, to check
-	 * if an item exists in the inventory before making a sale.
-	 */
+	//Method checkForItem takes itemID and returns the itemID if it's in the inventory
 	public Item checkForItem(int itemID) {
-
-	}
-	/*
-	 * getItemByID which returns Item and takes an int called itemID. It iterates
-	 * over the inventory and if it finds an Item with the given ID, it returns the
-	 * item. Otherwise, it returns null. Ensure that the Item’s properties can only
-	 * be modified by the Store class (how?).
-	 */
-
-	public int getItemByID(int itemID) {
-		Item item=null;
-		for(int i=0;i<inventory.size();i++) {
-			if(i.getItemID()==itemID) {
-				item = i;
+		Item item = null; //we set the placeholder item to null
+		for(Item items: inventory) { //Going through the inventory looking for the items id
+			if(items.getItemID().equals(itemID)) { //if the item has the same id as itemid
+				item = item.makeCopy(); //we make a copy of this item id incase we need it for a sale
 			}
 		}
 		return item;
 	}
-
+	//Method getItemByID takes an itemID and returns the itemID
+	public Item getItemByID(int itemID) {
+		Item item = null;
+		for(Item items: inventory) { //Goes through the inventory looking for the given itemID
+			if(items.getItemID() == itemID) { //if the item id is a match with the itemid in the inventory
+				item = items;
+			}
+		}
+		return item; // we return the item
+	}
+	//Getter method for totalSalesValue
+	public double totalSalesValue() {
+		return totalSalesValue;
+	}
+	//Getter method for totalNumberOfSales
+	public int totalNumberOfSales() {
+		return totalNumberOfSales;
+	}
+	//Ovveride annotation to output the total sales value, total number of sales, and stores current inventory.
+	@Override
+	public String toString() {
+		return "Total sales value: " + totalSalesValue + "\nTotal number of sales: " + totalNumberOfSales + "\nCurrent Inventory: " + getInventoryString();
+	}
+}
 }
